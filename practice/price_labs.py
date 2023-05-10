cc46db0f70174fe95bfa5d171ac112f70ff59ced
ankitforcodes <ankitforcodes@gmail.com>
2023-05-10 21:59:46 +0530

    practice

---
+++ practice/price_labs.py
@@ -0,0 +1,23 @@
+my_list= [1,4,2,-2,-9,10,2,12,2,-4,-4,-4,-4,2,6,7] 
+#my_list = [0,-1,-2,3,-4,-5,-6,7,-8,9,-10]
+peak = my_list[0]
+index = 0
+output = []
+
+for x in range(len(my_list)):
+	if x == 0:
+		continue
+	elif (my_list[x]*my_list[x-1] > 0):
+		if peak < 0 and my_list[x] < peak:
+			peak = my_list[x]
+			index = x
+
+		if peak > 0 and my_list[x] > peak:
+			peak = my_list[x]
+			index = x
+	else:
+		output.append((index,peak))
+		peak = my_list[x]
+		index = x
+
+print(output)
