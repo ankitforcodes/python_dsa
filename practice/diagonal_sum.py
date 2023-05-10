eb3abc668548667836d52777850c063410f394ea
ankitforcodes <ankitforcodes@gmail.com>
2023-05-10 14:10:01 +0530

    Update diagonal_sum.py

--- practice/diagonal_sum.py
+++ practice/diagonal_sum.py
@@ -3,10 +3,20 @@
 
 myList2D= [[1,2,3],[4,5,6],[7,8,9]] 
  
+# O(n*n)
+def diagonal_sum(my_array):
+	for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            if i == j:
+                answer += matrix[i][j]
+    return answer
 
 
+# O(n)
+def diagonal_sum1(my_array):
+	answer = 0
+	for i in range(len(matrix)):
+		answer += matrix[i][i]
 
 
-
- 
 diagonal_sum(myList2D) # 15
